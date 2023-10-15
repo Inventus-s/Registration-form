2ec228669eb1b82dad54c9c317f345639dc14b2b
Inventus-s <handle.extras@gmail.com>
2023-10-15 22:54:41 +0530

    home page

---
+++ home.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BuildPro</title>
+    <link rel="stylesheet" href="home.css">
+    <style>
+        a:hover {
+            color: grey;
+        }
+    </style>
+</head>
+
+<body>
+    <!-- Header -->
+    <header>
+        <nav class="main-nav">
+            <!-- left Section -->
+            <div class="left-head">
+                <img src="images/BuildProLogo.png" alt="logo">
+            </div>
+            <!-- right Section -->
+            <div class="right-head">
+                <!-- nav bar links -->
+                <div class="navbar-up">
+                    <div class="home">
+                        <i class="fa-solid fa-house"></i>
+                        <a href="#">Home</a>
+                    </div>
+                    <div class="register">
+                        <i class="fa-solid fa-address-card"></i>
+                        <a href="php/userForm.php">Register</a>
+                    </div>
+                    <div class="login">
+                        <i class="fa-solid fa-user"></i>
+                        <a href="php/login.php">Login</a>
+                    </div>
+                    <div class="about">
+                        <i class="fa-solid fa-globe"></i>
+                        <a href="#">About us</a>
+                    </div>
+
+                </div>
+                <div class="navbar-down">
+                    <div>
+                        <i class="fa-brands fa-linkedin"></i>
+                        <a href="https://www.linkedin.com/in/inventus/">Linkedin</a>
+                    </div>
+                    <div>
+                        <i class="fa-brands fa-github"></i>
+                        <a href="https://github.com/Inventus-s">GitHub</a>
+                    </div>
+                    <div>
+                        <i class="fa-regular fa-id-badge"></i>
+                        <a href="https://wa.me/918076872127">Contact Us</a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+        <nav class="sub-nav">
+            <a href="">Architecture & Engineering</a>
+            <a href="">Building Material</a>
+            <a href="">Construction Equipment</a>
+            <a href="">Contractors</a>
+            <a href="">Doors & Windows</a>
+            <a href="">Electricity</a>
+            <a href="">Flooring Material</a>
+            <a href="">Paint</a>
+            <a href="">Plumbing</a>
+        </nav>
+    </header>
+    <!-- hero section -->
+    <div class="hero-section">
+        <img src="https://images.pexels.com/photos/1009928/pexels-photo-1009928.jpeg?auto=compress&cs=tinysrgb&w=6000" alt="contruction">
+        <p>Building Materials</p>
+    </div>
+    <!-- Content Section -->
+    <div class="content-section">
+        <div class="article">
+            <div class="left-article">
+                <h1>Welcome to <span style="color:#2563eb;">BuildPro</span> - Your Trusted Source for Quality Building Materials</h1>
+                <p>At <span style="color:rgb(37, 99, 235,.8);">BuildPro</span>, we believe in the power of solid foundations, quite literally! Our mission is to provide you with top-notch building materials that form the basis of every remarkable structure. Whether you're a DIY enthusiast or a seasoned contractor, we're here to cater to all your building material needs.</p>
+            </div>
+            <div class="right-article">
+                <img src="https://images.pexels.com/photos/8961065/pexels-photo-8961065.jpeg?auto=compress&cs=tinysrgb&w=1000" alt="construction-img">
+            </div>
+        </div>
+        <div class="article">
+            <div class="right-article">
+                <img src="https://images.pexels.com/photos/2760243/pexels-photo-2760243.jpeg?auto=compress&cs=tinysrgb&w=1000" alt="construction-img">
+            </div>
+            <div class="left-article">
+                <h1>Our Commitment to Quality</h1>
+                <p>We understand the importance of using high-quality materials in every project. That's why we carefully curate and source our products from reputable manufacturers to ensure durability, reliability, and performance. With <span style="color:#2563eb;">BuildPro</span>, you can have peace of mind knowing you're investing in excellence.</p>
+            </div>
+        </div>
+        <div class="article">
+            <div class="left-article">
+                <h1>Explore Our Range of Building Materials</h1>
+                <p>Discover a comprehensive selection of building materials that fit your requirements and budget. From cement and steel to roofing materials and flooring solutions, we've got you covered. Browse through our diverse categories to find the perfect materials for your next project.</p>
+            </div>
+            <div class="right-article">
+                <img src="https://images.pexels.com/photos/585419/pexels-photo-585419.jpeg?auto=compress&cs=tinysrgb&w=1000" alt="construction-img">
+            </div>
+        </div>
+        <div class="article">
+            <div class="right-article">
+                <img src="https://images.pexels.com/photos/73833/weld-hot-soldering-radio-welder-73833.jpeg?auto=compress&cs=tinysrgb&w=1000" alt="construction-img">
+            </div>
+            <div class="left-article">
+                <h1>Why Choose <span style="color:#2563eb;">BuildPro</span>?</h1>
+                <p>
+                <ol class="li-article">
+                    <li> <span style="color:rgb(0, 0, 0, .8)">Exceptional Customer Service:</span> Our dedicated team is always ready to assist you, providing expert advice and helping you make informed decisions. </li>
+                    <li><span style="color:rgb(0, 0, 0, .8)">Competitive Pricing:</span> We offer competitive prices without compromising on quality, ensuring you get the best value for your investment. </li>
+                    <li><span style="color:rgb(0, 0, 0, .8)">Easy Ordering and Delivery:</span> Our user-friendly website allows for a seamless ordering process, and we ensure timely and efficient delivery to your doorstep.
+                    </li>
+                    <li><span style="color:rgb(0, 0, 0, .8)">Expertise and Knowledge:</span> Backed by years of experience, we are here to guide you through the material selection process, ensuring you get what's best for your project.</li>
+                </ol>
+                </p>
+            </div>
+        </div>
+        <div class="article">
+            <div class="left-article">
+                <h1>Start Building with Confidence</h1>
+                <p>At <span style="color:rgb(37, 99, 235,.8);">BuildPro</span>, we're not just providing building materials; we're empowering you to create structures that stand the test of time. Explore our extensive range of high-quality building materials and start your project with confidence today. <br> <br> For inquiries, feel free to <a style="color:rgb(37, 99, 235,.8); text-decoration:none;" href="#">contact us</a> or <a style="color:rgb(37, 99, 235,.8);text-decoration:none;" href="#">place an order</a> right away!</p>
+            </div>
+            <div class="right-article">
+                <img src="https://images.pexels.com/photos/8159/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1000" alt="construction-img">
+            </div>
+        </div>
+    </div>
+
+
+    <!-- Footer -->
+    <br><br><br>
+    <footer>Copyright | BuildPro | All Right Reserved</footer>
+    <script src="https://kit.fontawesome.com/4dfe9f290e.js" crossorigin="anonymous"></script>
+</body>
+
+</html>
