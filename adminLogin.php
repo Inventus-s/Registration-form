8a25bb9103c29566d6dc19136f468c3986eb0a66
Inventus-s <handle.extras@gmail.com>
2023-10-22 23:07:56 +0530

    adminLogin

---
+++ adminLogin.php
@@ -0,0 +1,27 @@
+<?php
+if (isset($_POST['username']) && !empty($_POST['username']) && isset($_POST['password']) && !empty($_POST['password'])) {
+    require_once('connection.php');
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $query = "select * from admin where username = '$username' and password = '$password'";
+
+    $response = mysqli_query($conn,$query );
+    if(mysqli_num_rows($response) > 0) {
+        $userData = mysqli_fetch_assoc($response);
+        session_start();
+        $_SESSION['userData'] = $userData;
+        echo 'true';
+        // header('location:userDetails.php');
+    } else {
+        echo 'false';
+        // header('location:login.php');
+    }
+} else {
+    session_start();
+    unset($_SESSION['userData']);
+    session_destroy();
+    header('location:login.php');
+} 
+
+?>
